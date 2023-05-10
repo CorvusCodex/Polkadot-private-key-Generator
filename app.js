42da9cef026b8803d99083f8df17edd5436d2a27
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2023-05-10 11:55:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+
+//Creaded by: Corvus Codex
+//Github: https://github.com/CorvusCodex/
+//Licence : MIT License
+
+//Support my work:
+//BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3
+//ETH & BNB: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0
+//Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex
+
 const schnorrkel = require('@polkadot/wasm-crypto/schnorrkel');
 const { u8aToHex } = require('@polkadot/util');
 const { encodeAddress } = require('@polkadot/keyring');
