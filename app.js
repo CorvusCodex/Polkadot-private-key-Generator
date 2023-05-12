ed25b9bacfe6282bc9da7f8ef007ad706d000c7d
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2023-05-12 22:19:21 +0200

    Update app.js
    Rewrited code.

--- app.js
+++ app.js
@@ -8,32 +8,24 @@
 //ETH & BNB: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0
 //Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex
 
-const schnorrkel = require('@polkadot/wasm-crypto/schnorrkel');
-const { u8aToHex } = require('@polkadot/util');
-const { encodeAddress } = require('@polkadot/keyring');
+const { Keyring } = require('@polkadot/keyring');
+const { randomAsU8a, cryptoWaitReady } = require('@polkadot/util-crypto');
 
-async function generatePolkadotKeyPair() {
-  // Generate a random seed
-  const seed = schnorrkel.randomBytes(32);
-
-  // Generate a key pair from the seed
-  const keyPair = schnorrkel.keypairFromSeed(seed);
-
-  // Extract the private and public keys
-  const privateKey = u8aToHex(keyPair.slice(0, 64));
-  const publicKey = u8aToHex(keyPair.slice(64));
-
-  // Encode the public key as a Polkadot address
-  const address = encodeAddress(publicKey);
-
-  return { privateKey, publicKey, address };
-}
-
+// Define an asynchronous main function
 async function main() {
-  const { privateKey, publicKey, address } = await generatePolkadotKeyPair();
-  console.log(`Private key: ${privateKey}`);
-  console.log(`Public key: ${publicKey}`);
-  console.log(`Address: ${address}`);
+  // Wait for the WASM interface to be ready
+  await cryptoWaitReady();
+  // Create a new Keyring instance with the sr25519 key type
+  const keyring = new Keyring({ type: 'sr25519' });
+  // Generate a random seed using the randomAsU8a function
+  const seed = randomAsU8a(32);
+  // Add a new key pair to the keyring using the generated seed
+  const pair = keyring.addFromSeed(seed);
+
+  // Print the private key as a hexadecimal string
+  console.log(`Private Key: ${seed.toString('hex')}`);
+  // Print the address of the key pair
+  console.log(`Address: ${pair.address}`);
 }
 
 main();
